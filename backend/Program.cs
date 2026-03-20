39ffa4b2d82ea93820f46c849a2f9011e885b1b6
Boripan Dechthongchai <boripan.dtc@gmail.com>
2026-03-20 19:39:25 +0700

    API Test

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,41 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+using var client = new HttpClient();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/weather/data", async () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var response = await client.GetAsync("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&timezone=Asia%2FBangkok&forecast_days=3");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (response.IsSuccessStatusCode)
+    {
+        return "ยิงสำเร็จ";
+    }
+
+    return "ยิงไม่สำเร็จ";
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
